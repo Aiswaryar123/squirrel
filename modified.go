7d38a8a116d51be5fe8fc458ca715e1ef42538c1
Aiswarya r <aiswaryarathee123@gmail.com>
2025-10-15 09:41:44 +0530

    add preprocess function

--- modified.go
+++ modified.go
@@ -8,59 +8,37 @@ import (
 	"slices"
 )
 
+
 type Entry struct {
 	Events   []string `json:"events"`
 	Squirrel bool     `json:"squirrel"`
 }
-
 type Counts struct {
-	n00, n01, n10, n11 uint
+	n00 uint
+	n01 uint
+	n10 uint
+	n11 uint
+}
+type MaxMin struct {
+	max      float64
+	min      float64
+	maxEvent string
+	minEvent string
 }
 
-func main() {
-	data, err := os.ReadFile("sample.json")
-	if err != nil {
-		fmt.Println("Could not open the file:", err)
-		return
-	}
-
-	var journalEntry []Entry
-	err = json.Unmarshal(data, &journalEntry)
-	if err != nil {
-		fmt.Println("File could not unmarshal:", err)
-		return
-	}
-
-	//fmt.Println("Journal data loaded", journalEntry)
-
-	corrValues := make(map[string]float64)
-
-	for _, entry := range journalEntry {
-		for _, e := range entry.Events {
-			if _, exists := corrValues[e]; !exists {
-				counts := getCounts(journalEntry, e)
-				corr := phi(counts)
-				corrValues[e] = corr
-			}
+func preprocess(journalEntries []Entry) []Entry {
+	var journal []Entry
+	for _, entry := range journalEntries {
+		hasPeanuts := slices.Contains(entry.Events, "peanuts")
+		notBrushedTeeth := !slices.Contains(entry.Events, "brushed teeth")
+		if hasPeanuts && notBrushedTeeth {
+			entry.Events = append(entry.Events, "dirty teeth")
+			journal = append(journal, entry)
+		} else {
+			journal = append(journal, entry)
 		}
 	}
-
-	max := -1.0
-	min := 1.0
-	var maxEvent, minEvent string
-	for key, value := range corrValues {
-		if value > max {
-			max = value
-			maxEvent = key
-		}
-		if value < min {
-			min = value
-			minEvent = key
-		}
-	}
-
-	fmt.Printf("Most Positively Correlated event: %s (%f)\n", maxEvent, max)
-	fmt.Printf("Most Negatively Correlated event: %s (%f)\n", minEvent, min)
+	return journal
 }
 
 func getCounts(journalEntries []Entry, event string) Counts {
@@ -78,27 +56,73 @@ func getCounts(journalEntries []Entry, event string) Counts {
 			} else {
 				n00++
 			}
+
 		}
 	}
-	return Counts{n00, n01, n10, n11}
+
+	return Counts{n00: n00, n01: n01, n10: n10, n11: n11}
 }
-
-func phi(count Counts) float64 {
-	n00 := float64(count.n00)
-	n01 := float64(count.n01)
-	n10 := float64(count.n10)
-	n11 := float64(count.n11)
-
+func phi(counts Counts) float64 {
+	n00 := float64(counts.n00)
+	n01 := float64(counts.n01)
+	n10 := float64(counts.n10)
+	n11 := float64(counts.n11)
 	n1_ := n10 + n11
 	n0_ := n00 + n01
 	n_1 := n01 + n11
-	n_0 := n00 + n10
-
+	n_0 := n10 + n00
 	num := (n11 * n00) - (n10 * n01)
 	den := math.Sqrt(n1_ * n0_ * n_1 * n_0)
-
-	if den == 0 {
-		return 0
-	}
 	return num / den
+}
+func getCorrelations(journalEntries []Entry) map[string]float64 {
+	corrValues := make(map[string]float64)
+	for _, entry := range journalEntries {
+		for _, e := range entry.Events {
+			counts := getCounts(journalEntries, e)
+			corr := phi(counts)
+			corrValues[e] = corr
+
+		}
+	}
+	return corrValues
+}
+func getMaxMin(corrValues map[string]float64) MaxMin {
+	var results MaxMin
+	results.max = -1.0
+	results.min = 1.0
+	for key, value := range corrValues {
+		if value > results.max {
+			results.max = value
+			results.maxEvent = key
+		}
+		if value < results.min {
+			results.min = value
+			results.minEvent = key
+		}
+
+	}
+	return results
+}
+
+func main() {
+	data, err := os.ReadFile("sample.json")
+	if err != nil {
+		fmt.Println("Could not open json file: ", err)
+	}
+
+	var journalEntries []Entry
+	err = json.Unmarshal(data, &journalEntries)
+	if err != nil {
+		fmt.Println("Could not unmarshal json file: ", err)
+	}
+	//fmt.Println(journalEntries)
+	journalEntries = preprocess(journalEntries)
+	
+	corrValues := getCorrelations(journalEntries)
+	results := getMaxMin(corrValues)
+
+	fmt.Printf("Most Positively Correlated event: %s (%f)\n", results.maxEvent, results.max)
+	fmt.Printf("Most Negatively Correlated event: %s (%f)\n", results.minEvent, results.min)
+
 }
